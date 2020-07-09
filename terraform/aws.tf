7c66d293935e474c3dc9860ef74e20c5d34bca9b
David Vargas <dvargas92495@gmail.com>
2020-07-09 10:26:19 -0400

    Point site to netlify

--- terraform/aws.tf
+++ terraform/aws.tf
@@ -23,69 +23,6 @@ resource "aws_route53_zone" "zone" {
     force_destroy = false
 }
 
-resource "aws_s3_bucket" "client" {
-  bucket = local.domain
-  force_destroy = true
-  acl    = "private"
-
-  tags = {
-    Application = "Website"
-  }
-
-  website {
-    index_document = "index.html"
-    error_document = "index.html"
-  }
-}
-
-data "aws_iam_policy_document" "client" {
-  statement {
-    sid = "PublicReadObject"
-    principals {
-      type = "*"
-      identifiers = ["*"]
-    }
-    actions = ["s3:GetObject"]
-    resources = ["${aws_s3_bucket.client.arn}/*"]
-  }
-}
-
-resource "aws_s3_bucket_policy" "client" {
-  bucket = aws_s3_bucket.client.id
-  policy = data.aws_iam_policy_document.client.json
-}
-
-resource "aws_s3_bucket" "www" {
-  bucket = "www.${local.domain}"
-  acl    = "private"
-  force_destroy = true
-  tags = {
-    Application = "Website"
-  }
-
-  website {
-    redirect_all_requests_to = "https://${local.domain}"
-  }
-}
-
-resource "aws_s3_bucket_public_access_block" "client" {
-  bucket = aws_s3_bucket.client.id
-
-  block_public_acls       = true
-  block_public_policy     = false
-  ignore_public_acls      = true
-  restrict_public_buckets = false
-}
-
-resource "aws_s3_bucket_public_access_block" "www" {
-  bucket = aws_s3_bucket.www.id
-
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
-
 resource "aws_acm_certificate" "cert" {
   domain_name       = local.domain
   validation_method = "DNS"
@@ -127,78 +64,13 @@ resource "aws_acm_certificate_validation" "validation" {
   ]
 }
 
-resource "aws_cloudfront_distribution" "s3_distribution" {
-  origin {
-    domain_name = aws_s3_bucket.client.bucket_regional_domain_name
-    origin_id   = local.s3_origin
-  }
-
-  enabled             = true
-  is_ipv6_enabled     = true
-  comment             = ""
-  default_root_object = "index.html"
-
-  aliases = [local.domain]
-
-  default_cache_behavior {
-    allowed_methods  = ["GET", "HEAD"]
-    cached_methods   = ["GET", "HEAD"]
-    target_origin_id = local.s3_origin
-    compress         = true
-
-    forwarded_values {
-      query_string = false
-
-      cookies {
-        forward = "none"
-      }
-    }
-
-    viewer_protocol_policy = "redirect-to-https"
-    min_ttl                = 0
-    default_ttl            = 86400
-    max_ttl                = 31536000
-  }
-
-  tags = {
-    Application = "Website"
-  }
-
-  viewer_certificate {
-    acm_certificate_arn = aws_acm_certificate.cert.arn
-    ssl_support_method = "sni-only"
-    minimum_protocol_version = "TLSv1.2_2018"
-  }
-
-  restrictions {
-    geo_restriction {
-      restriction_type = "none"
-    }
-  }
-
-  custom_error_response {
-    error_caching_min_ttl = 300
-    error_code = 403
-    response_code = 200
-    response_page_path = "/index.html"
-  }
-
-  custom_error_response {
-    error_caching_min_ttl = 300
-    error_code = 404
-    response_code = 200
-    response_page_path = "/index.html"
-  }
-}
-
 resource "aws_route53_record" "A" {
   zone_id = aws_route53_zone.zone.zone_id
   name    = local.domain
   type    = "A"
 
   alias {
-    name                   = aws_cloudfront_distribution.s3_distribution.domain_name
-    zone_id                = aws_cloudfront_distribution.s3_distribution.hosted_zone_id
+    name                   = "compassionate-visvesvaraya-9e2bd6.netlify.app."
     evaluate_target_health = false
   }
 }
@@ -209,105 +81,7 @@ resource "aws_route53_record" "AAAA" {
   type    = "AAAA"
 
   alias {
-    name                   = aws_cloudfront_distribution.s3_distribution.domain_name
-    zone_id                = aws_cloudfront_distribution.s3_distribution.hosted_zone_id
-    evaluate_target_health = false
-  }
-}
-
-resource "aws_cloudfront_distribution" "s3_www_distribution" {
-  origin {
-    domain_name = aws_s3_bucket.www.bucket_regional_domain_name
-    origin_id   = "www-${local.s3_origin}"
-    
-    custom_origin_config {
-      http_port    = "80"
-      https_port   = "443"
-      origin_protocol_policy = "http-only"
-      origin_ssl_protocols = [
-        "TLSv1",
-        "TLSv1.1",
-        "TLSv1.2"
-      ]
-    }
-  }
-
-  enabled             = true
-  is_ipv6_enabled     = true
-  comment             = ""
-
-  aliases = ["www.${local.domain}"]
-
-  default_cache_behavior {
-    allowed_methods  = ["GET", "HEAD"]
-    cached_methods   = ["GET", "HEAD"]
-    target_origin_id = "www-${local.s3_origin}"
-
-    forwarded_values {
-      query_string = false
-
-      cookies {
-        forward = "none"
-      }
-    }
-
-    viewer_protocol_policy = "redirect-to-https"
-    min_ttl                = 0
-    default_ttl            = 86400
-    max_ttl                = 31536000
-  }
-
-  tags = {
-    Application = "Website"
-  }
-
-  viewer_certificate {
-    acm_certificate_arn = aws_acm_certificate.cert.arn
-    ssl_support_method = "sni-only"
-    minimum_protocol_version = "TLSv1.2_2018"
-  }
-
-  restrictions {
-    geo_restriction {
-      restriction_type = "none"
-    }
-  }
-
-  custom_error_response {
-    error_caching_min_ttl = 300
-    error_code = 403
-    response_code = 200
-    response_page_path = "/index.html"
-  }
-
-  custom_error_response {
-    error_caching_min_ttl = 300
-    error_code = 404
-    response_code = 200
-    response_page_path = "/index.html"
-  }
-}
-
-resource "aws_route53_record" "www-A" {
-  zone_id = aws_route53_zone.zone.zone_id
-  name    = "www.${local.domain}"
-  type    = "A"
-
-  alias {
-    name                   = aws_cloudfront_distribution.s3_www_distribution.domain_name
-    zone_id                = aws_cloudfront_distribution.s3_www_distribution.hosted_zone_id
-    evaluate_target_health = false
-  }
-}
-
-resource "aws_route53_record" "www-AAAA" {
-  zone_id = aws_route53_zone.zone.zone_id
-  name    = "www.${local.domain}"
-  type    = "AAAA"
-
-  alias {
-    name                   = aws_cloudfront_distribution.s3_www_distribution.domain_name
-    zone_id                = aws_cloudfront_distribution.s3_www_distribution.hosted_zone_id
+    name                   = "compassionate-visvesvaraya-9e2bd6.netlify.app."
     evaluate_target_health = false
   }
 }
