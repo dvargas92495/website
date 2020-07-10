a49d92d14052ea2145c2f9657f21d08f6f708f3d
David Vargas <dvargas92495@gmail.com>
2020-07-10 11:27:21 -0400

    Remove CNAMES

--- terraform/aws.tf
+++ terraform/aws.tf
@@ -23,47 +23,6 @@ resource "aws_route53_zone" "zone" {
     force_destroy = false
 }
 
-resource "aws_acm_certificate" "cert" {
-  domain_name       = local.domain
-  validation_method = "DNS"
-
-  tags = {
-    Application = "Website"
-  }
-
-  lifecycle {
-    create_before_destroy = true
-  }
-
-  subject_alternative_names = [
-    "www.${local.domain}"
-  ]
-}
-
-resource "aws_route53_record" "cert_validation" {
-  name    = aws_acm_certificate.cert.domain_validation_options.0.resource_record_name
-  type    = aws_acm_certificate.cert.domain_validation_options.0.resource_record_type
-  zone_id = aws_route53_zone.zone.id
-  records = ["${aws_acm_certificate.cert.domain_validation_options.0.resource_record_value}"]
-  ttl     = 60
-}
-
-resource "aws_route53_record" "www_cert_validation" {
-  name    = aws_acm_certificate.cert.domain_validation_options.1.resource_record_name
-  type    = aws_acm_certificate.cert.domain_validation_options.1.resource_record_type
-  zone_id = aws_route53_zone.zone.id
-  records = [aws_acm_certificate.cert.domain_validation_options.1.resource_record_value]
-  ttl     = 60
-}
-
-resource "aws_acm_certificate_validation" "validation" {
-  certificate_arn         = aws_acm_certificate.cert.arn
-  validation_record_fqdns = [
-    "${aws_route53_record.cert_validation.fqdn}",
-    "${aws_route53_record.www_cert_validation.fqdn}"
-  ]
-}
-
 resource "aws_iam_user" "davidvargas" {
   name = "davidvargas"
 }
