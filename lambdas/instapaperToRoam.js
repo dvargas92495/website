e8516397632c87188631fe44f67b538663098d57
David Vargas <dvargas92495@gmail.com>
2020-07-24 23:46:23 -0400

    Instapaper integration

--- lambdas/instapaperToRoam.js
+++ lambdas/instapaperToRoam.js
@@ -1,18 +1,46 @@
 const axios = require("axios");
-var ClientOAuth2 = require('client-oauth2')
+const crypto = require("crypto");
+const OAuth = require("oauth-1.0a");
 
-// https://www.npmjs.com/package/oauth-1.0a
-var instapaperOauth = new ClientOAuth2({
-  clientId: process.env.INSTAPAPER_CLIENT_ID,
-  clientSecret: process.env.INSTAPAPER_CLIENT_SECRET,
-  accessTokenUri: 'https://instapaper.com/api/1/oauth/access_token',
-  authorizationUri: 'https://instapaper.com/api/1/account/verify_credentials',
-  redirectUri: 'http://example.com/auth/github/callback',
-  scopes: ['notifications', 'gist']
+const oauth = OAuth({
+  consumer: {
+    key: process.env.INSTAPAPER_CLIENT_ID,
+    secret: process.env.INSTAPAPER_CLIENT_SECRET,
+  },
+  signature_method: "HMAC-SHA1",
+  hash_function(base_string, key) {
+    return crypto.createHmac("sha1", key).update(base_string).digest("base64");
+  },
+});
+
+const tokenRequestData = {
+  url: "https://instapaper.com/api/1/oauth/access_token",
+  method: "POST",
+  data: {
+    x_auth_username: process.env.INSTAPAPER_USERNAME,
+    x_auth_password: process.env.INSTAPAPER_PASSWORD,
+    x_auth_mode: "client_auth",
+  },
+};
+
+axios({
+  ...tokenRequestData,
+  form: oauth.authorize(tokenRequestData),
 })
+  .then(res => console.log(`TOKEN: ${res.data}`))
+  .catch(e => console.error(JSON.stringify(e.message)));
 
-axios
-  .post("https://instapaper.com/api/1/bookmarks/list")
-  .then(res => console.log(res.data));
+/*
+const requestData = {
+  url: "https://instapaper.com/api/1/bookmarks/list",
+  method: "POST",
+  data: {},
+};
 
-console.log();
+axios({
+  ...requestData,
+  headers: oauth.toHeader(oauth.authorize(requestData, {})),
+})
+  .then(res => console.log(res.data))
+  .catch(e => console.error(JSON.stringify(e.message)));
+*/
