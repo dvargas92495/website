5d312d2f992b791cc274946b333bb9a0867ca8e7
David Vargas <dvargas92495@gmail.com>
2020-08-12 18:20:07 -0400

    Testing sucks

--- gatsby-config.js
+++ gatsby-config.js
@@ -369,6 +369,7 @@ module.exports = {
           `gatsby-remark-prismjs`,
           `gatsby-remark-copy-linked-files`,
           `gatsby-remark-smartypants`,
+          "@weknow/gatsby-remark-twitter",
         ],
       },
     },
