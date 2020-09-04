82bdd08eb166e1fc3ccbafea39a70148684d5579
David Vargas <dvargas92495@gmail.com>
2020-09-03 21:46:56 -0400

    Closes #3

---
+++ src/pages/subscribe.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+import Container from "@material-ui/core/Container";
+import Typography from "@material-ui/core/Typography";
+import { colors } from "../utils/typography";
+
+const Subscribe = () => (
+  <Layout>
+    <SEO title="Subscribe" />
+    <Container maxWidth="md" style={{ textAlign: "center" }}>
+      <Typography
+        variant="h5"
+        style={{ color: colors.primary, paddingTop: 32, paddingBottom: 32 }}
+      >
+        Join the other 42 subscribers and stay up to date on what I'm creating!
+      </Typography>
+    </Container>
+  </Layout>
+);
+
+export default Subscribe;
