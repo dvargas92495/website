22539d1d9796c329c9aed3421b3cbf790a9b2d6d
David Vargas <dvargas92495@gmail.com>
2020-11-18 09:56:06 -0500

    Native to Roam

--- scripts/blog-post.js
+++ scripts/blog-post.js
@@ -1,9 +1,9 @@
-const fs = require('fs');
+const fs = require("fs");
 
 const [_, __, title, description, acknowledgement, tags] = process.argv;
 const time = new Date();
 time.setMilliseconds(0);
-time.setHours(time.getHours() - time.getTimezoneOffset()/60);
+time.setHours(time.getHours() - time.getTimezoneOffset() / 60);
 const format = `---
 title: "${title}"
 date: "${time.toISOString()}"
@@ -13,8 +13,15 @@ tags: ${tags}
 ---
 `;
 
-const padNum = num => num < 10 ? `0${num}` : num;
-const folder = `content/blog/${time.getFullYear()}${padNum(time.getMonth() + 1)}${padNum(time.getDate())}-${title.toLowerCase().replace(/ /g,'-').replace(/:/g, '').replace(/,/g, '')}`;
+const padNum = num => (num < 10 ? `0${num}` : num);
+const folder = `content/blog/${time.getFullYear()}${padNum(
+  time.getMonth() + 1
+)}${padNum(time.getDate())}-${title
+  .toLowerCase()
+  .replace(/ /g, "-")
+  .replace(/:/g, "")
+  .replace(/,/g, "")
+  .replace(/\./g, "")}`;
 
 fs.mkdirSync(folder);
 fs.writeFileSync(`${folder}/index.md`, format);
